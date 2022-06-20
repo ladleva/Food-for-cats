479e820c58b0c72b67951605d8de6f4a88af6bd7
ladleva <86316632+ladleva@users.noreply.github.com>
2022-06-20 19:15:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,14 @@ const clickClass = "clicked";
 [...allCards].forEach(function(main) {
     const card = main.querySelector(".product__item");
     const btn = main.querySelector(".item__link");
-    card.addEventListener("click", function() {
+
+    function change() {
         if (main.classList.contains(clickClass)) {
             main.classList.remove(clickClass);
         } else {
             main.classList.add(clickClass);
         }
-    });
-    btn.addEventListener("click", function() {
-        if (main.classList.contains(clickClass)) {
-            main.classList.remove(clickClass);
-        } else {
-            main.classList.add(clickClass);
-        }
-    });
+    };
+    card.addEventListener("click", change);
+    btn.addEventListener("click", change);
 });
